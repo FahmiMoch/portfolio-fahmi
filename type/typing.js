a5fb4b380d2b47644594834b12d02281cad23363
Mochamad Fahmi <151912818+FahmiMoch@users.noreply.github.com>
2025-07-25 00:25:56 +0700

    Add files via upload

---
+++ type/typing.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+          new Typed("#typed-output", {
+            strings: ["Information System Student","UI/UX Enthusiast", "Building Career as a Software Engineering"],
+            typeSpeed: 50,
+            backSpeed: 25,
+            backDelay: 2000,
+            loop: true,
+          });
+        });
+        
